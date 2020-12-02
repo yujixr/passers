836ed7d956a136685807c1dde044a9c122015380
YUJI <mail@yuji.ne.jp>
2020-12-02 20:56:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,30 +6,41 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const baseurl = new URL(request.url);
-  const url = `https://${baseurl.pathname.slice(1)}${baseurl.search}`;
+  try {
+    const baseurl = new URL(request.url);
+    const url = `https://${baseurl.pathname.slice(1)}${baseurl.search}`;
+    const headers = new Headers({
+      "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+      "Referer": "https://www.google.com/search"
+    });
+    const res = await fetch(url, { headers })
 
-  if (url == 'https://') {
-    return new Response('', {
+    const content_type = res.headers.get("content-type");
+    if ((content_type.indexOf("charset=") == -1 && content_type.indexOf("text/") == -1 && content_type.indexOf("javascript") == -1)
+      || content_type.indexOf("svg") != -1
+      || content_type.indexOf("woff") != -1) {
+      return res;
+    }
+
+    const html = (await res.text())
+      .replaceAll(`https://`, `https://passers.yuji.workers.dev/`)
+      .replaceAll(`http://`, `https://passers.yuji.workers.dev/`)
+      .replaceAll(`href="/`, `href="https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
+      .replaceAll(`href='/`, `href='https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
+      .replaceAll(`href=/`, `href=https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
+      .replaceAll(`src="/`, `src="https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
+      .replaceAll(`src='/`, `src='https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
+      .replaceAll(`src=/`, `src=https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
+      .replaceAll(/integrity="sha[\d]{3}-.{44,88}"/g, '')
+      .replaceAll(/integrity='sha[\d]{3}-.{44,88}'/g, '')
+      .replaceAll(/integrity=sha[\d]{3}-.{44,88}/g, '');
+
+    return new Response(html, {
+      headers: { 'content-type': content_type },
+    });
+  } catch (e) {
+    return new Response(e.name + e.message, {
       headers: { 'content-type': 'text/plain' },
     });
   }
-
-  const headers = new Headers({
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-    "Referer": "https://www.google.com"
-  });
-  const res = await fetch(url, { headers })
-  let html = (await res.text())
-    .replaceAll(`href="/`, `href="https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
-    .replaceAll(`href='/`, `href='https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
-    .replaceAll(`href=/`, `href=https://passers.yuji.workers.dev/${(new URL(url)).host}/`)
-    .replaceAll(`src="/`, `src="https://${(new URL(url)).host}/`)
-    .replaceAll(`src='/`, `src='https://${(new URL(url)).host}/`)
-    .replaceAll(`src=/`, `src=https://${(new URL(url)).host}/`)
-    .replaceAll(`<link>https://`, `<link>https://passers.yuji.workers.dev/`);
-
-  return new Response(html, {
-    headers: { 'content-type': res.headers.get("content-type") },
-  });
 }
